948e44d110d26785c206b389c125e08a631a5dbc
RiyaSingh-code <riyasingh192006@gmail.com>
2026-09-04 01:12:30 +0530

    Update products.py

--- products.py
+++ products.py
@@ -1,22 +1,114 @@
 products = [
+
+    # Laptops
     {
-        "name": "Boat Rockerz 450",
-        "price": 1499,
-        "rating": 4.3
+        "name": "HP Victus 15",
+        "price": 65000,
+        "rating": 4.5,
+        "category": "laptop",
+        "image": "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
     },
     {
-        "name": "JBL Tune 510BT",
-        "price": 2499,
-        "rating": 4.5
+        "name": "Lenovo IdeaPad Gaming 3",
+        "price": 62000,
+        "rating": 4.4,
+        "category": "laptop",
+        "image": "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
     },
     {
-        "name": "Noise Two Wireless",
+        "name": "ASUS TUF F15",
+        "price": 70000,
+        "rating": 4.7,
+        "category": "laptop",
+        "image": "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+    },
+    {
+        "name": "Acer Aspire 7",
+        "price": 55000,
+        "rating": 4.3,
+        "category": "laptop",
+        "image": "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+    },
+    {
+        "name": "Acer Aspire Lite",
+        "category": "laptop",
+        "price": 45000,
+        "rating": 4.2,
+        "image": "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+    },
+
+    # Smartphones
+    {
+        "name": "Samsung Galaxy M35",
+        "price": 18999,
+        "rating": 4.4,
+        "category": "smartphone",
+        "image": "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+    },
+    {
+        "name": "iQOO Z9",
+        "price": 19999,
+        "rating": 4.5,
+        "category": "smartphone","image": "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+    },
+    {
+        "name": "Nothing Phone 2a",
+        "price": 23999,
+        "rating": 4.6,
+        "category": "smartphone",
+        "image": "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+    },
+    {
+        "name": "Redmi Note 13",
+        "price": 17999,
+        "rating": 4.3,
+        "category": "smartphone",
+        "image": "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+    },
+
+    # Headphones
+    {
+        "name": "Sony WH-CH520",
+        "price": 3999,
+        "rating": 4.6,
+        "category": "headphones",
+        "image": "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+    },
+    {
+        "name": "Boat Rockerz 550",
         "price": 1999,
-        "rating": 4.2
+        "rating": 4.3,
+        "category": "headphones",
+         "image": "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
     },
     {
-    "name": "Sony WH-CH520",
-    "price": 3999,
-    "rating": 4.6
+        "name": "JBL Tune 760NC",
+        "price": 5499,
+        "rating": 4.5,
+        "category": "headphones",
+        "image": "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+    },
+
+    # Earbuds
+    {
+        "name": "Noise Buds VS104",
+        "price": 1299,
+        "rating": 4.2,
+        "category": "earbuds",
+        "image": "https://images.unsplash.com/photo-1588423771073-b8903fbb85b5"
+    },
+    {
+        "name": "OnePlus Nord Buds 2",
+        "price": 2799,
+        "rating": 4.4,
+        "category": "earbuds",
+        "image": "https://images.unsplash.com/photo-1588423771073-b8903fbb85b5"
+    },
+    {
+        "name": "Realme Buds Air 5",
+        "price": 3499,
+        "rating": 4.5,
+        "category": "earbuds",
+        "image": "https://images.unsplash.com/photo-1588423771073-b8903fbb85b5"
     }
 ]
