7cda08b70637f2195a94f3ecf4b5da634ceeed84
RiyaSingh-code <riyasingh192006@gmail.com>
2026-09-04 01:10:45 +0530

    Enhance product comparison with budget and category
    Refactor compare_products function to include budget filtering and category matching.

--- compare.py
+++ compare.py
@@ -1,7 +1,28 @@
-def compare_products(products):
-    print("\nComparing Products:")
+from products import products
+import re
+
+def compare_products(user_query):
+
+    matching_products = []
+
+    query = user_query.lower()
+
+    budget = None
+
+    numbers = re.findall(r"\d+", query)
+    if numbers:
+        budget = int(numbers[0])
 
     for product in products:
-        print(
-            f"{product['name']} | ₹{product['price']} | Rating: {product['rating']}"
-        )
+
+        if product["category"].lower() in query:
+
+            if budget is None or product["price"] <= budget:
+                matching_products.append(product)
+
+    matching_products.sort(
+        key=lambda x: x["rating"],
+        reverse=True
+    )
+
+    return matching_products
