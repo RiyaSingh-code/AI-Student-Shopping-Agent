a879b897174378972e460e459f4e281d892562ba
RiyaSingh-code <riyasingh192006@gmail.com>
2026-09-04 01:13:09 +0530

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,10 +1,41 @@
-from products import products
+from compare import compare_products
+from utils import extract_budget
 
-def search_product(query):
-    result = []
 
-    for product in products:
-        if query.lower() in product["name"].lower():
-            result.append(product)
+def get_product_category(query):
+    query = query.lower()
 
-    return result
+    if "laptop" in query:
+        return "laptop"
+    elif "phone" in query:
+        return "phone"
+    elif "headphone" in query:
+        return "headphones"
+
+    return query
+
+
+def recommend_product(query):
+    budget = extract_budget(query)
+
+    category = get_product_category(query)
+
+    products = compare_products(query)
+    print("Category:", category)
+    print("Products:", products)
+
+    if budget:
+        products = [
+            p for p in products
+            if p["price"] <= budget
+        ]
+
+    if not products:
+        return None
+
+    best_product = max(
+        products,
+        key=lambda x: x["rating"]
+    )
+
+    return best_product
